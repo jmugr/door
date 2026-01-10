a037b9ed5a4ad16d45699e15aa5ca2caaefc357d
jrenn3 <jrenn3@gmail.com>
2026-01-10 16:00:28 -0600

    Quartz sync: Jan 10, 2026, 4:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://jrenn3.github.io/quartz/",
+    baseUrl: "jrenn3.github.io/quartz/",
     ignorePatterns: [
       "private", 
       "templates", 
