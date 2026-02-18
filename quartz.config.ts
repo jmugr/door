ebd9ee2b8f3ad37cf191a0b1a9644de669db11d9
jrenn3 <jrenn3@gmail.com>
2026-02-17 18:29:40 -0600

    disable broken links

--- quartz.config.ts
+++ quartz.config.ts
@@ -74,7 +74,10 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.ObsidianFlavoredMarkdown({ 
+        enableInHtmlEmbed: false,
+        disableBrokenWikilinks: true,
+      }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
