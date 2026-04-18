f16172192ba8ace04ef802ede76717a66dbb1093
jrenn3 <jrenn3@gmail.com>
2026-04-18 16:52:03 -0500

    Update typography fonts in Quartz configuration - choose Merriweather for header

--- quartz.config.ts
+++ quartz.config.ts
@@ -31,8 +31,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Merriweather",
+        body: "Inter",
         code: "IBM Plex Mono",
       },
       colors: {
