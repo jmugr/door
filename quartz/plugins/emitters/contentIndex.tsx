8cfd63524db5a88d74ebe1080bc41a3404400712
jrenn3 <jrenn3@gmail.com>
2026-04-19 07:19:44 -0500

    Update default options to exclude changelog from graph, update static images

--- quartz/plugins/emitters/contentIndex.tsx
+++ quartz/plugins/emitters/contentIndex.tsx
@@ -38,7 +38,7 @@ const defaultOptions: Options = {
   rssFullHtml: false,
   rssSlug: "index",
   includeEmptyFiles: true,
-  excludeChangelogFromGraph: true,
+  excludeChangelogFromGraph: false,
 }
 
 const excludedGraphFileName = "Garage changelog.md"
